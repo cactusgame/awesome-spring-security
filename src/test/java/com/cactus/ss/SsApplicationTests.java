62ef3c4faf9d5bdc71560c7069d9a042c1f13a3a
ShuiNiu <pengxu.hpx@alibaba-inc.com>
2020-10-27 19:48:34 +0800

    init

---
+++ src/test/java/com/cactus/ss/SsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactus.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
